c810745a963946e724c12d6495000e6a18aefcb7
visualstudio <visualstudio@122-PC38>
2019-03-06 11:15:12 -0500

    lab 02

--- CSD3354-2-Assignment-2-c0732101/Program.cs
+++ CSD3354-2-Assignment-2-c0732101/Program.cs
@@ -7,29 +7,14 @@ namespace DelegatesAndEvents
         public delegate void MyDelegate();
         void Method1()
         {
-            // Monika Sharma Student id c0732101
-            //CSD3354 Section 2
-            //Assignment 2
-            //March 6,2019
             Console.WriteLine("Method1");
             Console.ReadLine();
         }
         public void Method2()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            myDelegate(50);
         }
 
-    }
-}
-namespace DelegatesAndEvents
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method2();
-        }
     }
 }

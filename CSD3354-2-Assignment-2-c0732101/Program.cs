38e41f7f8590974defddc6824afd5b4d886fe7fd
visualstudio <visualstudio@122-PC38>
2019-03-06 11:22:25 -0500

    lab 05

--- CSD3354-2-Assignment-2-c0732101/Program.cs
+++ CSD3354-2-Assignment-2-c0732101/Program.cs
@@ -1,41 +1,27 @@
-﻿using System;
-
-namespace DelegatesAndEvents
+﻿namespace DelegatesAndEvents
 {
-
-    public class DelegateExercises
-    {
-        public delegate int MyDelegate(int intValue);
-
-        public int Method1(int intMethod1)
-        {
-            return intMethod1 * 2;
-        }
-
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            System.Console.WriteLine(result1);
-            myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            System.Console.WriteLine(result2);
-        }
-    }
-
     public class Program
     {
         public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
-            Console.ReadLine();
+        }
+    }
 
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate();
+
+        void Method1()
+        {
+            System.Console.WriteLine("MyDelegate");
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }

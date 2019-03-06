95ea77bd20718024d336a77f879b223e2d387c7d
visualstudio <visualstudio@122-PC38>
2019-03-06 11:14:19 -0500

    lab 01

--- CSD3354-2-Assignment-2-c0732101/Program.cs
+++ CSD3354-2-Assignment-2-c0732101/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSD3354_2_Assignment_2_c0732101
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            // Monika Sharma Student id c0732101
+            //CSD3354 Section 2
+            //Assignment 2
+            //March 6,2019
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+
+    }
+}
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }

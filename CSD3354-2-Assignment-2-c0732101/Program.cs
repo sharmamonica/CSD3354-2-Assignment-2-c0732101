d8502c67e777a2f27fc31a981ba40e984caadfa4
visualstudio <visualstudio@122-PC38>
2019-03-06 11:39:12 -0500

    LAB 10

--- CSD3354-2-Assignment-2-c0732101/Program.cs
+++ CSD3354-2-Assignment-2-c0732101/Program.cs
@@ -12,28 +12,27 @@ namespace DelegatesAndEvents
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
             Console.ReadLine();
-
         }
     }
 
-    public delegate void MyDelegate(ref int intValue);
+    public delegate void MyDelegate();
 
     public class DelegateExercises
     {
-        void Method1(ref int intValue)
+        void Method1()
         {
-            intValue = intValue + 5;
-            System.Console.WriteLine("Method1 " + intValue);
+            System.Console.WriteLine("Method1");
+
+
         }
 
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = new MyDelegate(Method1);
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intParameter = 5;
-            myDelegate2(ref intParameter);
+            myDelegate();
+            System.Console.WriteLine(myDelegate.ToString());
         }
     }
+
 
 }
